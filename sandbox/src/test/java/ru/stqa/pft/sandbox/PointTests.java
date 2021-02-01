702e2138d27464ac230392666e4f12186bd77e80
terehovandrej <terehovandrej@gmail.com>
2021-02-01 22:19:39 +0300

    Задание 3

---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -0,0 +1,29 @@
+package ru.stqa.pft.sandbox;
+
+import org.testng.annotations.Test;
+
+public class PointTests {
+
+    @Test
+    public void testDistanceOneLine(){
+        Point p1 = new Point(-1, 3);
+        Point p2 = new Point(-1, -4);
+        assert Point.distance(p1, p2) == 7.0;
+    }
+
+    @Test
+    public void testDistanceDifLine(){
+        Point p1 = new Point(-1, 1);
+        Point p2 = new Point(3, 4);
+        assert Point.distance(p1, p2) == 5.0;
+    }
+
+    @Test
+    public void testDistanceZero(){
+        Point p1 = new Point(0, 0);
+        Point p2 = new Point(0, 0);
+        assert Point.distance(p1, p2) == 0.0;
+    }
+
+
+}
