27c06663d8a951c380b6d88650a70dbdc494375b
terehovandrej <terehovandrej@gmail.com>
2021-03-18 13:11:10 +0300

    Реализована проверка данных из бд удаления, модификации групп

--- addressbook-web-test/src/test/java/ru/stqf/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-test/src/test/java/ru/stqf/pft/addressbook/tests/GroupDeletionTests.java
@@ -17,7 +17,7 @@ public class GroupDeletionTests extends TestBase {
     @BeforeMethod
     public void ensurePreconditions() {
         app.goTo().groupPage();
-        if (app.group().all().size() == 0) {
+        if (app.db().groups().size() == 0) {
             app.group().create(new GroupData().withName("test1"));
         }
     }
@@ -25,10 +25,10 @@ public class GroupDeletionTests extends TestBase {
 
     @Test
     public void testGroupDeletion()throws Exception {
-        Groups before = app.group().all();
+        Groups before = app.db().groups();
         GroupData deletedGroup = before.iterator().next();
         app.group().delete(deletedGroup);
-        Groups after = app.group().all();
+        Groups after = app.db().groups();
         Assert.assertEquals(after.size(), before.size() - 1);
         assertThat(after, equalTo(before.without(deletedGroup)));
 
